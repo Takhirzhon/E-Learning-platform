6915415e3b6815ae5f554c52b5d3a82f602288fa
Takhirzhon <tashmatovtahir@gmail.com>
2023-07-04 13:15:15 +0600

    Initial commit

---
+++ app/src/main/java/com/tokhirzhon/learn/Login.kt
@@ -0,0 +1,13 @@
+package com.tokhirzhon.learn
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_layout)
+
+    }
+}
