207651b500cae12c4113e0e4e3f07f92f13cc3fd
Takhirzhon <tashmatovtahir@gmail.com>
2023-07-04 21:16:53 +0600

    Fix errors

--- app/src/main/java/com/tokhirzhon/learn/Login.kt
+++ app/src/main/java/com/tokhirzhon/learn/Login.kt
@@ -10,42 +10,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class Login : AppCompatActivity() {
-    private lateinit var gso: GoogleSignInOptions
-    private lateinit var gsc: GoogleSignInClient
-    private lateinit var name: TextView
-    private lateinit var email: TextView
-    private lateinit var signOutBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_layout)
+        setContentView(R.layout.main_activity)
 
-        name = findViewById(R.id.name)
-        email = findViewById(R.id.email)
-        signOutBtn = findViewById(R.id.signout)
 
-        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
-        gsc = GoogleSignIn.getClient(this, gso)
-
-        val acct = GoogleSignIn.getLastSignedInAccount(this)
-        if (acct != null) {
-            val personName = acct.displayName
-            val personEmail = acct.email
-            name.text = personName
-            email.text = personEmail
-        }
-
-        signOutBtn.setOnClickListener {
-            signOut()
-        }
-    }
-
-    fun signOut() {
-        gsc.signOut().addOnCompleteListener(this) { task ->
-            finish()
-            startActivity(Intent(this@Login, MainActivity::class.java))
-        }
 
     }
 }
--- app/src/main/java/com/tokhirzhon/learn/MainActivity.kt
+++ app/src/main/java/com/tokhirzhon/learn/MainActivity.kt
@@ -24,44 +24,5 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        googleBtn = findViewById(R.id.google_btn)
-
-        gso =
-            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
-        gsc = GoogleSignIn.getClient(this, gso)
-
-        val account: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
-        if (account != null) {
-            navigateToSecondActivity()
-        }
-
-        googleBtn.setOnClickListener {
-            signIn()
-        }
-    }
-
-    fun signIn() {
-        val intent = gsc.signInIntent
-        startActivityForResult(intent, 1000)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1000) {
-            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
-
-            try {
-                task.getResult(ApiException::class.java)
-                navigateToSecondActivity()
-            } catch (e: ApiException) {
-                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    fun navigateToSecondActivity() {
-        finish()
-        val intent: Intent = Intent(this, Login::class.java)
-        startActivity(intent)
     }
 }
