f729fa5d43bfa4af6dbef29945ed1e3974dfc54b
Takhirzhon <tashmatovtahir@gmail.com>
2023-07-04 14:10:10 +0600

    Activity

--- app/src/main/java/com/tokhirzhon/learn/MainActivity.kt
+++ app/src/main/java/com/tokhirzhon/learn/MainActivity.kt
@@ -1,13 +1,67 @@
 package com.tokhirzhon.learn
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var gso: GoogleSignInOptions
+    lateinit var gsc: GoogleSignInClient
+    lateinit var googleBtn: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        googleBtn = findViewById(R.id.google_btn)
+
+        gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        gsc = GoogleSignIn.getClient(this, gso)
+
+        val account: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null) {
+            navigateToSecondActivity()
+        }
+
+        googleBtn.setOnClickListener {
+            signIn()
+        }
+    }
+
+    fun signIn() {
+        val intent = gsc.signInIntent
+        startActivityForResult(intent, 1000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1000) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                task.getResult(ApiException::class.java)
+                navigateToSecondActivity()
+            } catch (e: ApiException) {
+                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun navigateToSecondActivity() {
+        finish()
+        val intent: Intent = Intent(this, Login::class.java)
+        startActivity(intent)
     }
 }
