fcf568cb4c01321f596a8354dc4687eadc1177b7
Takhirzhon <tashmatovtahir@gmail.com>
2023-07-11 02:01:52 +0600

    Initial Commit

--- app/src/main/java/com/tokhirzhon/learn/Login.kt
+++ app/src/main/java/com/tokhirzhon/learn/Login.kt
@@ -1,6 +1,8 @@
 package com.tokhirzhon.learn
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import com.tokhirzhon.learn.R
@@ -11,6 +13,11 @@ class Login : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signin_layout)
 
-
+        val forgotPassword = findViewById<TextView>(R.id.forgotPassword)
+        forgotPassword.setOnClickListener {
+            val intent = Intent(this, PasswordReset ::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/tokhirzhon/learn/PasswordReset.kt
@@ -0,0 +1,13 @@
+package com.tokhirzhon.learn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class PasswordReset : ComponentActivity () {
+    override fun onCreate(savedInstanceState: Bundle? ){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.password_reset)
+
+
+    }
+}
