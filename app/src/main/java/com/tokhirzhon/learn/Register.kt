9479c5721acf46f0897c0df1e0eacaad6ab38deb
Takhirzhon <tashmatovtahir@gmail.com>
2023-07-15 22:27:22 +0600

    Initial Commit

--- app/src/main/java/com/tokhirzhon/learn/Register.kt
+++ app/src/main/java/com/tokhirzhon/learn/Register.kt
@@ -24,8 +24,6 @@ class Register : ComponentActivity() {
 
         }
 
-
-
         val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 if(cards.visibility == View.VISIBLE) {
