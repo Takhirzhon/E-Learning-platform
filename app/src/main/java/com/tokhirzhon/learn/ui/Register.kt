b760c8690d127f935f0bb89985104af3556667b3
Takhirzhon <tashmatovtahir@gmail.com>
2023-08-07 23:53:53 +0600

    Update

--- app/src/main/java/com/tokhirzhon/learn/ui/Register.kt
+++ app/src/main/java/com/tokhirzhon/learn/ui/Register.kt
@@ -9,16 +9,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.tokhirzhon.learn.R
 
 class Register : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_layout)
 
+        auth = Firebase.auth
         val contin = findViewById<Button>(R.id.sign_btn)
         val cards = findViewById<CardView>(R.id.additionalFieldsCardView)
+
         contin.setOnClickListener {
             cards.visibility = View.VISIBLE
             contin.visibility = View.GONE
@@ -27,16 +35,20 @@ class Register : AppCompatActivity() {
 
         val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                if(cards.visibility == View.VISIBLE) {
+                if (cards.visibility == View.VISIBLE) {
                     cards.visibility = View.GONE
                     contin.visibility = View.VISIBLE
-                }
-                else {
+                } else {
                     val intent = Intent(this@Register, MainActivity::class.java)
                     startActivity(intent)
                 }
             }
         }
         onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+
+    private fun signUp() {
+
     }
 }
