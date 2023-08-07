fbead934fcf1df8866b06e8703a319c798a0c08d
Takhirzhon <tashmatovtahir@gmail.com>
2023-08-08 03:21:35 +0600

    Update

--- app/src/main/java/com/tokhirzhon/learn/ui/Register.kt
+++ app/src/main/java/com/tokhirzhon/learn/ui/Register.kt
@@ -5,25 +5,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.activity.ComponentActivity
+import android.widget.EditText
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
 import com.tokhirzhon.learn.R
 
 class Register : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+     var dataBase = FirebaseDatabase
+
 
     @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_layout)
 
-        auth = Firebase.auth
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
         val contin = findViewById<Button>(R.id.sign_btn)
         val cards = findViewById<CardView>(R.id.additionalFieldsCardView)
 
@@ -45,10 +47,23 @@ class Register : AppCompatActivity() {
             }
         }
         onBackPressedDispatcher.addCallback(this, callback)
+
     }
 
 
     private fun signUp() {
+        val email = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val password = findViewById<EditText>(R.id.editTextTextPassword)
+        val passwordSure = findViewById<EditText>(R.id.editTextTextPassword2)
+        val cityName = findViewById<EditText>(R.id.city_name)
+        val schoolName = findViewById<EditText>(R.id.school_name)
+        val schoolNumber = findViewById<EditText>(R.id.school_num)
+        val classNum = findViewById<EditText>(R.id.class_num)
 
     }
+
+    fun onClickSave(view: View){
+        String email
+    }
+
 }
