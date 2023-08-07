4b061e55a8bb459917f26bd877467267b70a6a06
Takhirzhon <tashmatovtahir@gmail.com>
2023-08-07 23:26:20 +0600

    Update

--- app/src/main/java/com/tokhirzhon/learn/ui/favourite/FavouriteFragment.kt
+++ app/src/main/java/com/tokhirzhon/learn/ui/favourite/FavouriteFragment.kt
@@ -12,8 +12,6 @@ class FavouriteFragment : Fragment() {
 
     private var _binding: FragmentFavouriteBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
