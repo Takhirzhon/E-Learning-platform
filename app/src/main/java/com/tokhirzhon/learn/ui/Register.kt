9728e818b4e4a9e9514b71fc92ad59cb18d15a68
Takhirzhon <tashmatovtahir@gmail.com>
2023-08-08 14:55:16 +0600

    Update

--- app/src/main/java/com/tokhirzhon/learn/ui/Register.kt
+++ app/src/main/java/com/tokhirzhon/learn/ui/Register.kt
@@ -1,16 +1,13 @@
 package com.tokhirzhon.learn.ui
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
-import androidx.compose.material3.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -20,20 +17,15 @@ import com.tokhirzhon.learn.model.User
 class Register : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var dataBase: FirebaseDatabase
-    private lateinit var users: DatabaseReference
+    private lateinit var dataBase: DatabaseReference
+    private val USER_KEY : String = "User"
 
-
-    @SuppressLint("CutPasteId")
-    override
-
-    fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_layout)
 
         auth = FirebaseAuth.getInstance()
-        dataBase = FirebaseDatabase.getInstance()
-        users = dataBase.getReference("Users")
+        dataBase = FirebaseDatabase.getInstance().getReference(USER_KEY)
 
         val contin = findViewById<Button>(R.id.contin)
         val cards = findViewById<CardView>(R.id.additionalFieldsCardView)
@@ -41,12 +33,13 @@ class Register : AppCompatActivity() {
         contin.setOnClickListener {
             cards.visibility = View.VISIBLE
             contin.visibility = View.GONE
-
         }
 
         val signUpIn = findViewById<Button>(R.id.sign)
         signUpIn.setOnClickListener {
             signUp()
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
         }
 
         val callback = object : OnBackPressedCallback(true) {
@@ -61,31 +54,20 @@ class Register : AppCompatActivity() {
             }
         }
         onBackPressedDispatcher.addCallback(this, callback)
-
     }
 
-
     private fun signUp() {
-        val email = findViewById<EditText>(R.id.editTextTextEmailAddress)
-        val password = findViewById<EditText>(R.id.editTextTextPassword)
-        val passwordSure = findViewById<EditText>(R.id.editTextTextPassword2)
-        val cityName = findViewById<EditText>(R.id.city_name)
-        val schoolName = findViewById<EditText>(R.id.school_name)
-        val schoolNumber = findViewById<EditText>(R.id.school_num)
-        val classNum = findViewById<EditText>(R.id.class_num)
+        val email = findViewById<EditText>(R.id.editTextTextEmailAddress).text.toString()
+        val password = findViewById<EditText>(R.id.editTextTextPassword).text.toString()
+        val passwordSure = findViewById<EditText>(R.id.editTextTextPassword2).text.toString()
+        val cityName = findViewById<EditText>(R.id.city_name).text.toString()
+        val schoolName = findViewById<EditText>(R.id.school_name).text.toString()
+        val schoolNumber = findViewById<EditText>(R.id.school_num).text.toString()
+        val classNum = findViewById<EditText>(R.id.class_num).text.toString()
 
-
-            auth.createUserWithEmailAndPassword(email.toString(), password.toString())
-                .addOnSuccessListener {
-                    val user = User()
-                    user.setEmail(email.toString())
-                    user.setPassword(password.toString())
-                    user.setCityName(cityName.toString())
-                    user.setSchoolName(schoolName.toString())
-
-                    users.child(user.getEmail()).setValue(user).addOnSuccessListener {
-                        Toast.makeText(this, "Вы добавлены", Toast.LENGTH_SHORT).show()
-                }
+        if (email.isNotEmpty() && password.isNotEmpty() && password == passwordSure) {
+            val user = User(email, password, cityName, schoolName, schoolNumber, classNum)
+            dataBase.push().setValue(user)
         }
     }
 }
