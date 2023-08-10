86c2ed2ed4d7cc94e7d55a464a9c96a7743eb293
Takhirzhon <tashmatovtahir@gmail.com>
2023-08-11 03:36:40 +0600

    Update

--- app/src/main/java/com/tokhirzhon/learn/ui/Login.kt
+++ app/src/main/java/com/tokhirzhon/learn/ui/Login.kt
@@ -4,18 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.ComponentActivity
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.tokhirzhon.learn.R
 
 class Login : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signin_layout)
+        auth = FirebaseAuth.getInstance()
+
 
         val cardViewReset = findViewById<CardView>(R.id.resetCardView) //Cardview сброс пароля
         val rememebered = findViewById<TextView>(R.id.remembered) // Вспомнил пароль назад
@@ -45,8 +50,28 @@ class Login : AppCompatActivity() {
         onBackPressedDispatcher.addCallback(this, callback)
 
         signInAcc.setOnClickListener {
-            val intent = Intent(this, MenuActivity::class.java)
-            startActivity(intent)
+            validateData()
         }
     }
+
+
+    private fun validateData() {
+        val email = findViewById<EditText>(R.id.editTextTextEmailAddress).text.toString().trim()
+        val password = findViewById<EditText>(R.id.editTextTextPassword).text.toString().trim()
+
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+            val firebaseUser = auth.currentUser!!
+
+            val ref = FirebaseDatabase.getInstance().getReference("users")
+            ref.child(firebaseUser.uid)
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+
+        }
+            .addOnFailureListener{
+                Toast.makeText(this, "Что то пошло не так", Toast.LENGTH_LONG).show()
+            }
+    }
 }
+
+
--- app/src/main/java/com/tokhirzhon/learn/ui/Register.kt
+++ app/src/main/java/com/tokhirzhon/learn/ui/Register.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -18,20 +19,36 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import com.tokhirzhon.learn.R
 import com.tokhirzhon.learn.model.User
+import kotlin.jvm.internal.Intrinsics.Kotlin
 
 class Register : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     lateinit var launcher: ActivityResultLauncher<Intent>
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var passwordSureEditText: EditText
+    private lateinit var cityNameEditText: EditText
+    private lateinit var schoolNameEditText: EditText
+    private lateinit var schoolNumberEditText: EditText
+    private lateinit var classNumEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_layout)
         auth = Firebase.auth
-
         auth = FirebaseAuth.getInstance()
+
+        emailEditText = findViewById(R.id.editTextTextEmailAddress)
+        passwordEditText = findViewById(R.id.editTextTextPassword)
+        passwordSureEditText = findViewById(R.id.editTextTextPassword2)
+        cityNameEditText = findViewById(R.id.city_name)
+        schoolNameEditText = findViewById(R.id.school_name)
+        schoolNumberEditText = findViewById(R.id.school_num)
+        classNumEditText = findViewById(R.id.class_num)
 
         launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
@@ -40,12 +57,12 @@ class Register : AppCompatActivity() {
                 if (account != null) {
                     account.idToken?.let { it1 -> firebaseAuthWithGoogle(it1) }
                 }
-            }catch (_: ApiException) {
+            } catch (_: ApiException) {
 
             }
         }
         val signInGoogleWrapper = findViewById<View>(R.id.google_sing_in_register_wrapper)
-        signInGoogleWrapper.setOnClickListener{
+        signInGoogleWrapper.setOnClickListener {
             signInWithGoogle()
         }
         val contin = findViewById<Button>(R.id.contin)
@@ -77,7 +94,9 @@ class Register : AppCompatActivity() {
         }
         onBackPressedDispatcher.addCallback(this, callback)
     }
+
     //кнопка конец
+
 
     private fun signUp() {
         val email = findViewById<EditText>(R.id.editTextTextEmailAddress).text.toString()
@@ -88,38 +107,52 @@ class Register : AppCompatActivity() {
         val schoolNumber = findViewById<EditText>(R.id.school_num).text.toString()
         val classNum = findViewById<EditText>(R.id.class_num).text.toString()
 
-
-
         if (email.isNotEmpty() && password.isNotEmpty() && password == passwordSure) {
-            val user = User(email, password, cityName, schoolName, schoolNumber, classNum)
+            auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+                val timeStamp = System.currentTimeMillis()
+                val uid = auth.uid
 
-            auth.createUserWithEmailAndPassword(email, password)
+                val hashMap: HashMap<String, Any?> = HashMap()
+                hashMap["uid"] = uid
+                hashMap["email"] = email
+                hashMap["cityName"] = cityName
+                hashMap["schoolName"] = schoolName
+                hashMap["schoolNum"] = schoolNumber
+                hashMap["classNum"] = classNum
+                hashMap["timeStamp"] = timeStamp
+
+                val ref = FirebaseDatabase.getInstance().getReference("users")
+                ref.child(uid!!).setValue(hashMap)
+                    .addOnSuccessListener {
+                        val intent = Intent(this, MenuActivity::class.java)
+                        startActivity(intent)
+                    }
+            }
         }
     }
-    private fun getClient() : GoogleSignInClient {
+
+
+    private fun getClient(): GoogleSignInClient {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-        return  GoogleSignIn.getClient(this, gso)
+        return GoogleSignIn.getClient(this, gso)
     }
 
-    private fun signInWithGoogle(){
+    private fun signInWithGoogle() {
         val signInClient = getClient()
         launcher.launch(signInClient.signInIntent)
     }
 
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential).addOnCompleteListener{
-            if(it.isSuccessful){
-                Log.d("My Log", "Вы вошли в аккаунт")
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
                 val intent = Intent(this, MenuActivity::class.java)
                 startActivity(intent)
             }
-            else {
-                Log.d("My Log", "Ошибка входа, попробуйте еще раз!")
-            }
+
         }
     }
 }
