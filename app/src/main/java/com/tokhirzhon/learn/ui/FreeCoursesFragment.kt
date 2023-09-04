63adfee57b882c5a972042e57782bb83e0567735
Takhirzhon <tashmatovtahir@gmail.com>
2023-09-04 15:27:00 +0600

    Update

--- app/src/main/java/com/tokhirzhon/learn/ui/FreeCoursesFragment.kt
+++ app/src/main/java/com/tokhirzhon/learn/ui/FreeCoursesFragment.kt
@@ -4,14 +4,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tokhirzhon.learn.R
+import com.tokhirzhon.learn.databinding.FragmentFreecoursesBinding
+import com.tokhirzhon.learn.databinding.FragmentHomeBinding
 
 class FreeCoursesFragment : Fragment() {
+
+    private var _binding: FragmentFreecoursesBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Здесь разместите логику и макет для фрагмента бесплатных курсов
-        return inflater.inflate(R.layout.fragment_freecourses, container, false)
+
+        _binding = FragmentFreecoursesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/tokhirzhon/learn/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tokhirzhon/learn/ui/home/HomeFragment.kt
@@ -1,16 +1,23 @@
 package com.tokhirzhon.learn.ui.home
 
+import FreeCoursesFragment
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
+import androidx.activity.OnBackPressedCallback
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.tokhirzhon.learn.R
+import com.tokhirzhon.learn.activity.MainActivity
 import com.tokhirzhon.learn.databinding.FragmentHomeBinding
+import com.tokhirzhon.learn.ui.connect.FragmentConnect
 
 class HomeFragment : Fragment() {
 
@@ -21,6 +28,7 @@ class HomeFragment : Fragment() {
     private lateinit var coursesPremiumBtn: ImageButton
     private lateinit var cardViewPremium: CardView
     private lateinit var premiumCoursesView: RecyclerView
+    private lateinit var connectBtn: Button
 
     private val binding get() = _binding!!
 
@@ -29,12 +37,12 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         // Найдите кнопки и CardView элементы
+        connectBtn = root.findViewById(R.id.connect_connect)
         coursesFreeBtn = root.findViewById(R.id.coursesFree)
         cardViewFree = root.findViewById(R.id.coursesCardViewFree)
         freeCoursesView = root.findViewById(R.id.freeCoursesView)
@@ -42,11 +50,23 @@ class HomeFragment : Fragment() {
         cardViewPremium = root.findViewById(R.id.coursesCardViewPremium)
         premiumCoursesView = root.findViewById(R.id.premiumCoursesView)
 
+
+        connectBtn.setOnClickListener {
+            val connect = FragmentConnect()
+            val transaction = requireFragmentManager().beginTransaction()
+            transaction.replace(R.id.nav_host_fragment_activity_menu, connect)
+            transaction.commit()
+        }
+
         // Установите слушатели для кнопок
         coursesFreeBtn.setOnClickListener {
             if (cardViewFree.visibility == View.VISIBLE) {
                 cardViewFree.visibility = View.GONE
             } else {
+                val connect = FreeCoursesFragment()
+                val transaction = requireFragmentManager().beginTransaction()
+                transaction.replace(R.id.nav_host_fragment_activity_menu, connect)
+                transaction.commit()
                 cardViewFree.visibility = View.VISIBLE
             }
         }
@@ -57,8 +77,20 @@ class HomeFragment : Fragment() {
             } else {
                 cardViewPremium.visibility = View.VISIBLE
             }
+
         }
 
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (cardViewFree.visibility == View.VISIBLE) {
+                    cardViewFree.visibility = View.GONE
+
+
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
         return root
     }
 
